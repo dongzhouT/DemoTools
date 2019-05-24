89b6c4df15eab51d314d3456ca0b0220e514b56f
dongzhouT <taodongzhou@126.com>
2019-05-24 13:29:37 +0800

    增加MVPdemo

---
+++ app/src/main/java/com/tao/demo/mvpDemo/loginDemo/UserInfoResultBean.java
@@ -0,0 +1,8 @@
+package com.tao.demo.mvpDemo.loginDemo;
+
+/**
+ * @author taodzh
+ * create at 2019/5/22
+ */
+public class UserInfoResultBean {
+}
---
+++ app/src/main/java/com/tao/demo/mvpDemo/loginDemo/entity/LoginResultBean.java
@@ -0,0 +1,8 @@
+package com.tao.demo.mvpDemo.loginDemo.entity;
+
+/**
+ * @author taodzh
+ * create at 2019/5/23
+ */
+public class LoginResultBean {
+}
